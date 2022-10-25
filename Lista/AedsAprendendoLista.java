7feeb48e512b0c693f5ddd3ea24a9dc0a7f81ad4
Thiago Salles <112223120+ThSFernandes@users.noreply.github.com>
2022-10-24 22:14:23 -0300

    Add files via upload

---
+++ Lista/AedsAprendendoLista.java
@@ -0,0 +1,30 @@
+
+package aedsaprendendolista;
+
+import java.util.Scanner;
+
+
+public class AedsAprendendoLista {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+        Lista.lista(4);;
+        Lista.insere(2);
+        Lista.insere(5);
+        Lista.insere(4);
+        Lista.insere(3);
+      
+        
+       // Imprime a lista:
+        Lista.imprime();
+        
+        
+          Lista.insere(5);
+        
+       
+    }
+    
+}
